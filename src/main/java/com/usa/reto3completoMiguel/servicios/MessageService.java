7ce5fa0dd8a8703912cc83b51c96eb5eb9f4711a
Miguel-Calderon-Gutierrez-mintic <93630964+Miguel-Calderon-Gutierrez-mintic@users.noreply.github.com>
2021-11-25 19:46:27 -0500

    tercer commit reto 4
    actualizado message service

--- src/main/java/com/usa/reto3completoMiguel/servicios/MessageService.java
+++ src/main/java/com/usa/reto3completoMiguel/servicios/MessageService.java
@@ -8,6 +8,7 @@ package com.usa.reto3completoMiguel.servicios;
 import com.usa.reto3completoMiguel.modelentidades.Message;
 import com.usa.reto3completoMiguel.repository.crud.MessageRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +22,50 @@ public class MessageService {
     @Autowired
     MessageRepository messageRepo;
 
-    public void guardar(Message Message) {
-        messageRepo.save(Message);
+    public Message guardar(Message message) {
+        if (message.getIdMessage()== null) {
+            return messageRepo.save(message);
+        } else {
+            Optional<Message> libaux = messageRepo.findById(message.getIdMessage());
+            if (libaux.isEmpty()) {
+                return messageRepo.save(message);
+            } else {
+                return message;
+            }
+        }
     }
 
     public List<Message> buscarTodo() {
         return (List<Message>) messageRepo.findAll();
+    }
+
+    public Message update(Message c) {
+        if (c.getIdMessage()!= null) {
+            Optional<Message> g = messageRepo.findById(c.getIdMessage());
+            if (!g.isEmpty()) {
+
+                if (c.getClient()!= null) {
+                    g.get().setClient(c.getClient());
+                }
+               if (c.getLib()!= null) {
+                    g.get().setLib(c.getLib());
+                }
+                if (c.getMessageText()!= null) {
+                    g.get().setMessageText(c.getMessageText());
+                }        
+                return messageRepo.save(g.get());
+            }
+        }
+        return c;
+    }
+
+    public boolean delete(Integer c) {
+        Optional<Message> g = messageRepo.findById(c);
+        if (!g.isEmpty()) {
+            messageRepo.deleteById(g.get().getIdMessage());
+            return true;
+        }
+        return false;
     }
 
 }

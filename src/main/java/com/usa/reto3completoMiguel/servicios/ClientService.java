ae13bdf02bf0133487e7d1b5d73e4eb874a7d07a
Miguel-Calderon-Gutierrez-mintic <93630964+Miguel-Calderon-Gutierrez-mintic@users.noreply.github.com>
2021-11-25 19:45:15 -0500

    commit 2 de reto 4
    actualizado cliente service

--- src/main/java/com/usa/reto3completoMiguel/servicios/ClientService.java
+++ src/main/java/com/usa/reto3completoMiguel/servicios/ClientService.java
@@ -5,9 +5,11 @@
  */
 package com.usa.reto3completoMiguel.servicios;
 
+
 import com.usa.reto3completoMiguel.modelentidades.Client;
 import com.usa.reto3completoMiguel.repository.crud.ClientRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +23,57 @@ public class ClientService {
     @Autowired
     ClientRepository ClientRepo;
 
-    public void guardar(Client client) {
-        ClientRepo.save(client);
+    public Client guardar(Client client) {
+          if (client.getIdClient()== null) {
+            return ClientRepo.save(client);
+        } else {
+            Optional<Client> libaux = ClientRepo.findById(client.getIdClient());
+            if (libaux.isEmpty()) {
+                return ClientRepo.save(client);
+            } else {
+                return client;
+            }
+        }
     }
 
     public List<Client> buscarTodo() {
         return (List<Client>) ClientRepo.findAll();
     }
 
+    public Client update(Client c) {
+        if (c.getIdClient()!= null) {
+            Optional<Client> g = ClientRepo.findById(c.getIdClient());
+            if (!g.isEmpty()) {
+                if (c.getAge() != null) {
+                    g.get().setAge(c.getAge());
+                }
+                if (c.getEmail()!= null) {
+                    g.get().setEmail(c.getEmail());
+                }
+                if (c.getMessages() != null) {
+                    g.get().setMessages(c.getMessages());
+                }
+                if (c.getName() != null) {
+                    g.get().setName(c.getName());
+                }
+                if (c.getPassword() != null) {
+                    g.get().setPassword(c.getPassword());
+                }
+                if (c.getReservations() != null) {
+                    g.get().setReservations(c.getReservations());
+                }
+                return ClientRepo.save(g.get());
+            }
+        }
+        return c;
+    }
+
+    public boolean delete(Integer c) {
+        Optional<Client> g = ClientRepo.findById(c);
+        if (!g.isEmpty()) {
+            ClientRepo.deleteById(g.get().getIdClient());
+            return true;
+        }
+        return false;
+    }
 }

1018e01fada4e8bd5868a7870d9d0890f66754cb
Miguel-Calderon-Gutierrez-mintic <93630964+Miguel-Calderon-Gutierrez-mintic@users.noreply.github.com>
2021-11-25 19:53:02 -0500

    sexto commit reto 4
    controlador de Category

--- src/main/java/com/usa/reto3completoMiguel/webcontroladores/CategoryRestController.java
+++ src/main/java/com/usa/reto3completoMiguel/webcontroladores/CategoryRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  *
@@ -36,24 +38,21 @@ public class CategoryRestController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity post(@RequestBody Category Category) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void post(@RequestBody Category Category) {
         Categoryserver.guardar(Category);
-        return ResponseEntity.status(201).build();
+        
     }
 
-    @GetMapping("/{id}")
-    public Object get(@PathVariable String id) {
-        return null;
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
-        return null;
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void put(@RequestBody Category category) {
+        Categoryserver.update(category);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable String id) {
-        return null;
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        Categoryserver.delete(id);
     }
-
 }

86b96b5706e37ebf294ee2cec88043cf6c70eac6
Miguel-Calderon-Gutierrez-mintic <93630964+Miguel-Calderon-Gutierrez-mintic@users.noreply.github.com>
2021-11-25 19:57:19 -0500

    noveno commit
    cambios reservation controller

--- src/main/java/com/usa/reto3completoMiguel/webcontroladores/ReserrvationRestController.java
+++ src/main/java/com/usa/reto3completoMiguel/webcontroladores/ReserrvationRestController.java
@@ -1,17 +1,17 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package com.usa.reto3completoMiguel.webcontroladores;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.usa.reto3completoMiguel.modelentidades.Reservation;
-import com.usa.reto3completoMiguel.servicios.ReservationService;
+import com.usa.reto3completoMiguel.modelentidades.Lib;
+import com.usa.reto3completoMiguel.servicios.libService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,43 +19,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  *
  * @author USER
  */
 @RestController
-@RequestMapping("/api/Reservation")
-public class ReserrvationRestController {
+@RequestMapping("/api/Lib")
+public class libRestController {
 
     @Autowired
-    ReservationService Reservationserver;
+    libService libserver;
 
-    @GetMapping("/all")    
-    public List<Reservation> list() {        
-        List lista =Reservationserver.buscarTodo();     
-        return lista;
+    @GetMapping("/all")
+    public List<Lib> list() {
+        return libserver.buscarTodo();
     }
 
     @PostMapping("/save")
-    public ResponseEntity post(@RequestBody Reservation Reservation) {
-        Reservationserver.guardar(Reservation);
-        return ResponseEntity.status(201).build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public void post(@RequestBody Lib lib) {
+        libserver.guardar(lib);
     }
+    
 
-    @GetMapping("/{id}")
-    public Object get(@PathVariable String id) {
-        return null;
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
-        return null;
+    @PutMapping("/update")
+     @ResponseStatus(HttpStatus.CREATED)
+    public void put(@RequestBody Lib lib) {
+        libserver.update(lib);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable String id) {
-        return null;
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+         libserver.delete(id);
     }
 
 }

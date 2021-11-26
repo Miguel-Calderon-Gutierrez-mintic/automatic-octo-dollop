8eb7338d43a989dabb292e7c452188ab62a3b60e
Miguel-Calderon-Gutierrez-mintic <93630964+Miguel-Calderon-Gutierrez-mintic@users.noreply.github.com>
2021-11-25 19:50:49 -0500

    quinto commit reto 4
    lib services actualizado

--- src/main/java/com/usa/reto3completoMiguel/servicios/libService.java
+++ src/main/java/com/usa/reto3completoMiguel/servicios/libService.java
@@ -20,29 +20,66 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 @Service
 public class libService {
+
     @Autowired
     libRepository libRepo;
-    
-    
-    public Lib guardar(Lib lib){ 
-        if (lib.getId()==null) {
-            libRepo.save(lib);
-        }else{
+
+    public Lib guardar(Lib lib) {
+        if (lib.getId() == null) {
+            return libRepo.save(lib);
+        } else {
             Optional<Lib> libaux = libRepo.findById(lib.getId());
             if (libaux.isEmpty()) {
                 return libRepo.save(lib);
-            }else{
+            } else {
                 return lib;
             }
         }
-        return null;
     }
-    
-    
-    public List<Lib>buscarTodo(){
+
+    public List<Lib> buscarTodo() {
         return (List<Lib>) libRepo.findAll();
     }
-    
-    
-    
+
+    public Lib update(Lib c) {
+        if (c.getId() != null) {
+            Optional<Lib> g = libRepo.findById(c.getId());
+            if (!g.isEmpty()) {
+
+                if (c.getCapacity() != null) {
+                    g.get().setCapacity(c.getCapacity());
+                }
+                if (c.getCategory() != null) {
+                    g.get().setCategory(c.getCategory());
+                }
+                if (c.getDescription() != null) {
+                    g.get().setDescription(c.getDescription());
+                }
+                if (c.getMessages() != null) {
+                    g.get().setMessages(c.getMessages());
+                }
+                if (c.getName() != null) {
+                    g.get().setName(c.getName());
+                }
+                if (c.getReservations() != null) {
+                    g.get().setReservations(c.getReservations());
+                }
+                if (c.getTarget() != null) {
+                    g.get().setTarget(c.getTarget());
+                }
+                return libRepo.save(g.get());
+            }
+        }
+        return c;
+    }
+
+    public boolean delete(Integer c) {
+        Optional<Lib> g = libRepo.findById(c);
+        if (!g.isEmpty()) {
+            libRepo.deleteById(g.get().getId());
+            return true;
+        }
+        return false;
+    }
+
 }
